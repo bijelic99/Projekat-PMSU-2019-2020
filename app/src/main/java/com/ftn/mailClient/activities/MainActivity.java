c9a29922a3b45056cce686e2abda36ac484ae678
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-04-17 01:23:42 +0200

    Radjeno na izgledu

--- app/src/main/java/com/ftn/mailClient/activities/MainActivity.java
+++ app/src/main/java/com/ftn/mailClient/activities/MainActivity.java
@@ -2,9 +2,13 @@ package com.ftn.mailClient.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ftn.mailClient.R;
+import com.ftn.mailClient.activities.emailsActivity.EmailsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, EmailsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ftn/mailClient/activities/emailsActivity/EmailsActivity.java
@@ -0,0 +1,17 @@
+package com.ftn.mailClient.activities.emailsActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ftn.mailClient.R;
+
+public class EmailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout
+        .activity_emails);
+    }
+}
