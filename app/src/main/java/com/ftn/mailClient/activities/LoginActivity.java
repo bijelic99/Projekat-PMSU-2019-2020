1405563c1701a647a0abe417c366a04039e088ce
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-04-16 15:40:02 +0200

    Napravljen izgled LoginActivity

---
+++ app/src/main/java/com/ftn/mailClient/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.ftn.mailClient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ftn.mailClient.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/ftn/mailClient/activities/MainActivity.java
+++ app/src/main/java/com/ftn/mailClient/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.ftn.mailClient.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ftn.mailClient.R;
 
@@ -12,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(clickListener);
+
     }
+
+    private View.OnClickListener clickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            Intent loginActivityIntent = new Intent(MainActivity.this, LoginActivity.class);
+            //loginActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(loginActivityIntent);
+        }
+    };
+
 }
