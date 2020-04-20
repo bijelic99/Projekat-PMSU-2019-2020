fcb2d184ab1c503cc88064cb001624d69116dd9f
Dejan Bijelic <bijelic.99.dejan@hotmail.rs>
2020-04-20 13:08:19 +0200

    Zapoceta implementacija

---
+++ app/src/main/java/com/ftn/mailClient/activities/foldersActivity/FoldersActivity.java
@@ -0,0 +1,16 @@
+package com.ftn.mailClient.activities.foldersActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ftn.mailClient.R;
+
+public class FoldersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_folders);
+    }
+}
