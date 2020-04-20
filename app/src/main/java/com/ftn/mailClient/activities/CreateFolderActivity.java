fac7b7edba5b4f1d8d9d9c9fadfd49c7e9f2ca22
svetozarb999@gmail.com <svetozarb999@gmail.com>
2020-04-20 18:27:54 +0200

    Izmjenjen CreateFolderActivity

---
+++ app/src/main/java/com/ftn/mailClient/activities/CreateFolderActivity.java
@@ -0,0 +1,37 @@
+package com.ftn.mailClient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ftn.mailClient.R;
+import com.ftn.mailClient.activities.emailsActivity.EmailsActivity;
+
+public class CreateFolderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_folder);
+
+        Button b = findViewById(R.id.buttonLogin);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickHandler(v);
+            }
+        });
+    }
+    public void clickHandler(View v) {
+        Intent intent = new Intent(this, EmailsActivity.class);
+        startActivity(intent);
+    }
+
+    //Disable back button
+    @Override
+    public void onBackPressed() { }
+
+}
