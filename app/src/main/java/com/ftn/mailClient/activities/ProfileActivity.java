29bf105def69b28dc6f0a8ad7fa69d9df04504bd
Svetozar99 <svetozarb999@gmail.com>
2020-06-25 22:19:41 +0000

    popravljen izgled profila

--- app/src/main/java/com/ftn/mailClient/activities/ProfileActivity.java
+++ app/src/main/java/com/ftn/mailClient/activities/ProfileActivity.java
@@ -2,10 +2,14 @@ package com.ftn.mailClient.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -26,36 +30,32 @@ public class ProfileActivity extends AppCompatActivity {
 
         Button bu = findViewById(R.id.buttonLogout);
 
-
-        bu.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                clickHandler(v);
-            }
-        });
-
         Button addRuleBtn = findViewById(R.id.add_rule_button);
         addRuleBtn.setOnClickListener(v -> {
             AddRuleDialog addRuleDialog = new AddRuleDialog();
             addRuleDialog.show(getSupportFragmentManager(), "Add rule dialog");});
-            
-        /*ImageButton butt = findViewById(R.id.addTagButton);
-        butt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clickHandler1(v);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.profile_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.buttonLogout:{
+                goToLogin();
             }
-        });*/
+        }
+        return super.onOptionsItemSelected(item);
     }
 
-    private void clickHandler(View v) {
+    public void goToLogin(){
         Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-    }
-
-    private void clickHandler1(View v){
-        Intent intent = new Intent(this, TagsActivity.class);
-        startActivity(intent);
+        this.startActivity(intent);
     }
 }
